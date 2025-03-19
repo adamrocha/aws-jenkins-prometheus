3efe69c8873beae3455a752c8c339b23001a8581
Adam Rocha <adamrocha13@gmail.com>
2025-03-19 10:46:22 -0700

    add instance column

--- awscli/fetch-ec2-instances.sh
+++ awscli/fetch-ec2-instances.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[Tags[?Key=='Name'].Value | [0], PrivateIpAddress, PublicIpAddress]" --output table
+aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name'].Value | [0], PrivateIpAddress, PublicIpAddress]" --output table
