9e6104ca607b25b82bd555008e8cc5e68947b2e5
Adam Rocha <adamrocha13@gmail.com>
2025-03-23 04:39:32 -0700

    include headers and clean syntax

--- awscli/fetch-ec2-instances.sh
+++ awscli/fetch-ec2-instances.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name'].Value | [0], PrivateIpAddress, PublicIpAddress]" --output table
+aws ec2 describe-instances \
+    --query "Reservations[].Instances[].{Name:Tags[?Key=='Name']|[0].Value,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}" \
+    --output table
