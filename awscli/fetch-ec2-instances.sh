fc017a768304a7bbcf7593744a52a897f9ffaf2c
Adam Rocha <adamrocha13@gmail.com>
2025-04-26 12:34:08 -0700

    filter running instances only

--- awscli/fetch-ec2-instances.sh
+++ awscli/fetch-ec2-instances.sh
@@ -2,4 +2,5 @@
 
 aws ec2 describe-instances \
     --query "Reservations[].Instances[].{Name:Tags[?Key=='Name']|[0].Value,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}" \
+    --filters Name=instance-state-name,Values=running \
     --output table
