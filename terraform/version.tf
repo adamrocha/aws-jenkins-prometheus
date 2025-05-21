ee4d3aa66c07843bb86c0032f2ef2a975c488a44
Adam Rocha <adamrocha13@gmail.com>
2025-05-21 10:08:45 -0700

    Add Terraform configuration for AWS provider

---
+++ terraform/version.tf
@@ -0,0 +1,11 @@
+// Define Terrraform Backend and Providers
+terraform {
+  required_version = "> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
