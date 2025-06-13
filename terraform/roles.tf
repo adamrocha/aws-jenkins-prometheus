4ab00deeb2c792a5a65bc9bfae9456e2fd55db78
Adam Rocha <adamrocha13@gmail.com>
2025-06-12 23:11:52 -0700

    Add SSM role and instance profile for EC2 instances

---
+++ terraform/roles.tf
@@ -0,0 +1,28 @@
+# SSM Role & Instance Profile
+resource "aws_iam_role" "ssm_ec2_role" {
+  name = "ssm-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core_attach" {
+  role       = aws_iam_role.ssm_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ssm-ec2-profile"
+  role = aws_iam_role.ssm_ec2_role.name
+}
+
