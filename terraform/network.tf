3d6fa8d2fe3f7c9f7d5e45f2daf44b6c927f87ec
Adam Rocha <adamrocha13@gmail.com>
2025-06-13 13:07:40 -0700

    Refactor network configuration by removing unused resources and reordering security group definitions

--- terraform/network.tf
+++ terraform/network.tf
@@ -31,12 +31,6 @@ resource "aws_subnet" "public_subnet" {
 // Routing table for public subnet
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.base_vpc.id
-  /*
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.base_ig.id
-  }
-  */
   tags = {
     Name = "public-rt"
   }
@@ -54,35 +48,7 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-/*
-# Elastic IP
-resource "aws_eip" "public_ip" {
-  domain = "vpc"
-}
 
-# Associate EIP to the ENI
-resource "aws_eip_association" "prom_eip" {
-  allocation_id        = aws_eip.public_ip.id
-  network_interface_id = aws_network_interface.prometheus_nic.id
-}
-
-# Associate EIP to the ENI
-resource "aws_eip_association" "jenkins_eip" {
-  allocation_id        = aws_eip.public_ip.id
-  network_interface_id = aws_network_interface.jenkins_nic.id
-}
-/*
-// Set new main_route_table as main
-resource "aws_main_route_table_association" "default" {
-  vpc_id         = aws_vpc.base_vpc.id
-  route_table_id = aws_route_table.public_rt.id
-}
-# Associate the public subnet with the route table
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-*/
 resource "aws_security_group_rule" "base_sg_internet_to_ec2_icmp" {
   security_group_id = aws_security_group.base_sg_ec2.id
   description       = "Allow ICMP access from the Internet"
@@ -101,20 +67,6 @@ resource "aws_security_group_rule" "base_sg_all_outbound" {
   to_port           = "0"
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "base_sg_ec2" {
-  description = "Base security group for EC2 instances"
-  name        = "base-sg-ec2"
-  vpc_id      = aws_vpc.base_vpc.id
-
-  ingress {
-    description = "Allow SSH access from the Internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "prometheus_sg_front_end" {
@@ -173,14 +125,19 @@ resource "aws_security_group" "jenkins_sg_front_end" {
   }
 }
 
+
+resource "aws_security_group" "base_sg_ec2" {
+  description = "Base security group for EC2 instances"
+  name        = "base-sg-ec2"
+  vpc_id      = aws_vpc.base_vpc.id
+}
 /*
-resource "aws_security_group_rule" "exporter_sr_internet_to_front_end_9100" {
-  description       = "Allow Node Exporter access from the Internet on port 9100"
-  security_group_id = aws_security_group.exporter_sg_front_end.id
-  type              = "ingress"
-  from_port         = 9100
-  to_port           = 9100
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ingress {
+    description = "Allow SSH access from the Internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 */
