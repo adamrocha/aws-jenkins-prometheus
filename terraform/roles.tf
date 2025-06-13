b91d25ae54819521b051bd867e875be01984567b
Adam Rocha <adamrocha13@gmail.com>
2025-06-13 13:07:11 -0700

    Refactor SSM role and instance profile naming for consistency

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,28 +1,23 @@
-# SSM Role & Instance Profile
-resource "aws_iam_role" "ssm_ec2_role" {
+resource "aws_iam_role" "ssm_role" {
   name = "ssm-ec2-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Effect = "Allow"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_core_attach" {
-  role       = aws_iam_role.ssm_ec2_role.name
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "ssm-ec2-profile"
-  role = aws_iam_role.ssm_ec2_role.name
+  name = "ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
 }
-
