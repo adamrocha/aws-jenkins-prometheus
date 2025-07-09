cc8aa3e9827b7581070e44ff1f9c1a766f651966
Adam Rocha <adamrocha13@gmail.com>
2025-07-09 03:22:51 -0700

    Refactor VPC and IAM roles for flow logs, update security group rules, and enhance logging configuration

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,11 +1,72 @@
 resource "aws_vpc" "base_vpc" {
-  # checkov:skip=CKV2_AWS_11: fix later
+  # checkov:skip=CKV2_AWS_12: SSM remote access
   cidr_block           = "172.21.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames = false
+  enable_dns_support   = false
   tags = {
     Name = "base-vpc"
   }
+}
+
+# Create an IAM role for VPC flow logs
+resource "aws_iam_role" "vpc_flow_logs_role" {
+  name = "vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs_policy" {
+  name = "vpc-flow-logs-policy"
+  role = aws_iam_role.vpc_flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/flow-logs/base-vpc"
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+  retention_in_days = 365
+}
+
+
+# Enable VPC flow logs
+resource "aws_flow_log" "vpc_flow_logs" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_logs_role.arn
+  vpc_id               = aws_vpc.base_vpc.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
 }
 
 // Internet Gateway
@@ -18,6 +79,7 @@ resource "aws_internet_gateway" "base_ig" {
 
 // AWS Public Subnet
 resource "aws_subnet" "public_subnet" {
+  # checkov:skip=CKV_AWS_130: SSM Remote Access
   vpc_id                  = aws_vpc.base_vpc.id
   cidr_block              = "172.21.0.0/24"
   availability_zone       = "us-east-1c"
@@ -49,95 +111,28 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group_rule" "base_sg_internet_to_ec2_icmp" {
-  security_group_id = aws_security_group.base_sg_ec2.id
-  description       = "Allow ICMP access from the Internet"
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "base_sg_all_outbound" {
-  security_group_id = aws_security_group.base_sg_ec2.id
-  description       = "Allow all outbound traffic to the Internet"
-  type              = "egress"
-  from_port         = "0"
-  to_port           = "0"
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "prometheus_sg_front_end" {
-  name        = "prometheus-sg-front-end"
-  description = "Security group for Prometheus front-end access"
-  vpc_id      = aws_vpc.base_vpc.id
-
+resource "aws_default_security_group" "restrict_default" {
+  vpc_id = aws_vpc.base_vpc.id
+  /*
   ingress {
-    description = "Allow Prometheus UI access from the Internet"
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
+    self      = true
+    from_port = 0
+    to_port   = 0
+    protocol    = "-1" # all protocols
+    cidr_blocks = []
+  }
+  */
+  egress {
+    self        = true
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # all protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_security_group" "exporter_sg_front_end" {
-  name        = "exporter-sg-front-end"
-  description = "Security group for Exporter front-end access"
-  vpc_id      = aws_vpc.base_vpc.id
-
-  ingress {
-    description = "Security group for Exporter front-end access"
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "grafana_sg_front_end" {
-  name        = "grafana-sg-front-end"
-  description = "Security group for Grafana front-end access"
-  vpc_id      = aws_vpc.base_vpc.id
-
-  ingress {
-    description = "Allow Grafana UI access from the Internet"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "jenkins_sg_front_end" {
-  name        = "jenkins-sg-front-end"
-  description = "Security group for Jenkins front-end access"
-  vpc_id      = aws_vpc.base_vpc.id
-
-  ingress {
-    description = "Allow Jenkins UI access from the Internet"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "Restrict Default SG"
   }
 }
 
 
-resource "aws_security_group" "base_sg_ec2" {
-  description = "Base security group for EC2 instances"
-  name        = "base-sg-ec2"
-  vpc_id      = aws_vpc.base_vpc.id
-}
-/*
-  ingress {
-    description = "Allow SSH access from the Internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-*/
